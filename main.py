a449d43eaef2310e1e56ac48e553495357e69de4
AT2A <artintonekaboni@gmail.com>
2024-08-06 21:20:37 -0400

    google

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import yfinance as yf
 from bs4 import BeautifulSoup
-
-
+import requests
+htlm_texts = requests.get('https://www.google.com/finance/quote/AAPL:NASDAQ').text
+soup = BeautifulSoup(htlm_texts, 'html.parser')
+News = soup.find('div', class_ = 'P6K39c')
+print(News.text)
 def news_org(ticker):
     
     tick = yf.Ticker(ticker)
