7fe433b52eadab9be4ad4988357215e046fcd981
AT2A <94756585+AT2A@users.noreply.github.com>
2024-08-07 14:39:47 -0400

    google finance

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import yfinance as yf
 from bs4 import BeautifulSoup
 import requests
-htlm_texts = requests.get('https://www.google.com/finance/quote/AAPL:NASDAQ').text
+htlm_texts = requests.get('https://www.google.com/finance/quote/NVDA:NASDAQ').text
 soup = BeautifulSoup(htlm_texts, 'html.parser')
-News = soup.find('div', class_ = 'P6K39c')
-print(News.text)
+infoList = soup.find_all('div', class_ = 'P6K39c')
+
+stockInfo = {
+  "prevClose":infoList[0].text,
+  "dayRange":infoList[1].text,
+  "yearRange":infoList[2].text,
+  "marketCap":infoList[3].text,
+  "avgVolume":infoList[4].text,
+  "PE":infoList[5].text,
+  "divYield":infoList[6].text,
+  "exchange":infoList[7].text
+  }
+
+print(stockInfo.get("marketCap"))
+
+#use google to get basic stock info
+#use stockAnaylsis to get forecast info
+#use reddit too
 def news_org(ticker):
     
     tick = yf.Ticker(ticker)
