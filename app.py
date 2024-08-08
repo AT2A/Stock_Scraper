3e3b09b1baef8a6d251f25368885b50c9188a789
AT2A <artintonekaboni@gmail.com>
2024-08-08 16:17:27 -0400

    stock analysis website

--- app.py
+++ app.py
@@ -7,19 +7,30 @@ import tkinter as tk
 root=tk.Tk()
 root.geometry("600x400")
 stock_var=tk.StringVar()
-
+exchange_var = tk.StringVar()
 
 
 def submit():
     name=stock_var.get()
+    exchange = exchange_var.get()
     main.news_org(name)
+    main.stock_info(name, exchange)
     stock_var.set("")
+    exchange_var.set("")
     
 stock_label = tk.Label(root, text = 'Stock Ticker', font=('calibre',10, 'bold'))
 stock_entry = tk.Entry(root,textvariable = stock_var, font=('calibre',10,'normal'))
+
+exchange_label = tk.Label(root, text = 'Exchange', font=('calibre',10, 'bold'))
+exchange_entry = tk.Entry(root,textvariable = exchange_var, font=('calibre',10,'normal'))
+
 sub_btn=tk.Button(root,text = 'Submit', command = submit)
+
 stock_label.grid(row=0,column=0)
 stock_entry.grid(row=0,column=1)
+exchange_label.grid(row=1,column=0)
+exchange_entry.grid(row=1,column=1)
+
 sub_btn.grid(row=2,column=1)
 
 
--- main.py
+++ main.py
@@ -1,32 +1,63 @@
 import yfinance as yf
 from bs4 import BeautifulSoup
 import requests
-htlm_texts = requests.get('https://www.google.com/finance/quote/NVDA:NASDAQ').text
-soup = BeautifulSoup(htlm_texts, 'html.parser')
-infoList = soup.find_all('div', class_ = 'P6K39c')
-stockAnalysisHTML = requests.get('https://stockanalysis.com/stocks/aapl/forecast/').text
-anaylsisSoup = BeautifulSoup(stockAnalysisHTML, 'html.parser')
-
-analName = anaylsisSoup.find('div', class_ = 'analyst-name svelte-1c46ly0')
-print(analName.a.text)
-stockInfo = {
-  "prevClose":infoList[0].text,
-  "dayRange":infoList[1].text,
-  "yearRange":infoList[2].text,
-  "marketCap":infoList[3].text,
-  "avgVolume":infoList[4].text,
-  "PE":infoList[5].text,
-  "divYield":infoList[6].text,
-  "exchange":infoList[7].text
-  }
-
-
 #use google to get basic stock info
 #use stockAnaylsis to get forecast info
-    #stock price forecast
-    #asnaylst ratings
+
     #anaylist ratings
 #use reddit too
+
+#Stock Analysis Website ----------------------------------------------------------------------------------------------
+#Class of the info of each stock analyst
+class analyst:
+  
+  def _init_(self):
+    self.name = ''
+    self.firm = ''
+    self.rating = ''
+    self.action = ''
+    self.priceTarget = ''
+    self.upside = ''
+    self.date = ''
+    
+stockAnalysisHTML = requests.get('https://stockanalysis.com/stocks/aapl/ratings/').text
+anaylsisSoup = BeautifulSoup(stockAnalysisHTML, 'html.parser')
+table = anaylsisSoup.find('tbody' , class_ = "svelte-1c46ly0")
+allAnalyst = table.find_all('tr')
+#List that stores each of the analyst objects and the loop, that loops through the websites table and extracts the data
+analystList = []
+for index,x in enumerate(allAnalyst):
+  analystList.append(analyst())
+  analystList[index].name = x.find('div', class_ = 'analyst-name svelte-1c46ly0').text
+  analystList[index].firm = x.find('td', class_ = 'desktop-only max-w-[120px] text-smaller svelte-1c46ly0').text
+  analystList[index].rating = x.find('td', class_ = 'desktop-only text-sm svelte-1c46ly0').span.text
+  analystList[index].action = x.find('td', class_ = 'desktop-only text-smaller svelte-1c46ly0').text
+  analystList[index].priceTarget = x.find('td', class_ = 'desktop-only min-w-[80px] text-smaller svelte-1c46ly0').text
+  analystList[index].upside = x.find('td', class_ = 'desktop-only text-right text-smaller svelte-1c46ly0').text
+  analystList[index].date = x.find('td', class_ = 'whitespace-nowrap align-middle text-smaller font-semibold svelte-1c46ly0').text
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#Stock info from google finance
+def stock_info(ticker, exchange):
+  htlm_texts = requests.get(f'https://www.google.com/finance/quote/{ticker}:{exchange}').text
+  soup = BeautifulSoup(htlm_texts, 'html.parser')
+  infoList = soup.find_all('div', class_ = 'P6K39c')
+  stockInfo = {
+    "prevClose":infoList[0].text,
+    "dayRange":infoList[1].text,
+    "yearRange":infoList[2].text,
+    "marketCap":infoList[3].text,
+    "avgVolume":infoList[4].text,
+    "PE":infoList[5].text,
+    "divYield":infoList[6].text,
+    "exchange":infoList[7].text
+    }
+  print(stockInfo)
+
+#News on stock through yfinance
 def news_org(ticker):
     
     tick = yf.Ticker(ticker)
