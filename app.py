ba312dfd6051fb5f8eafceaa3aba98e529bf422a
AT2A <94756585+AT2A@users.noreply.github.com>
2024-08-08 22:53:46 -0400

    toText feature

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ root=tk.Tk()
 root.geometry("500x800")
 stock_var=tk.StringVar()
 exchange_var = tk.StringVar()
+analystList = []
+newsList = []
+def toText():
+    with 
+
 
 def submit():
     name=stock_var.get()
     exchange = exchange_var.get()
-    
+    newsList = main.news_org(name)
     main.stock_info(name, exchange)
+    analystList = main.stockAnalysis(name)
     stock_var.set("")
     exchange_var.set("")
     
@@ -27,7 +33,7 @@ exchange_label.grid(row=1,column=0)
 exchange_entry.grid(row=1,column=1)
 
 sub_btn.grid(row=2,column=1)
-print(main.news_org("aapl")[0].link)
+print([0].link)
 
 
 root.mainloop()
